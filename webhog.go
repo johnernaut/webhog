d0a8c4759c6a7ba089728b2bb5695a1a9ca7b968
John Johnson <john@johnjohnson.cc>
2014-05-22 11:54:05 -0500

    add version and travis yml

--- webhog.go
+++ webhog.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"github.com/go-martini/martini"
 	"github.com/johnernaut/webhog/webhog"
 	"github.com/johnernaut/webhog/webhog/router"
@@ -8,10 +10,20 @@ import (
 	_ "github.com/martini-contrib/gzip"
 	"github.com/martini-contrib/render"
 	"net/http"
+	"os"
 	"runtime"
 )
 
+const VERSION = "v0.1.0"
+
 func main() {
+	version := flag.Bool("version", false, "current version")
+	flag.Parse()
+	if *version {
+		fmt.Println(VERSION)
+		os.Exit(0)
+	}
+
 	// All the parallelism are belong to us!
 	runtime.GOMAXPROCS(runtime.NumCPU())
 
